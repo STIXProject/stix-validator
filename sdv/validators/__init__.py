d4749e52bf1761c341f62357560bca58ebbf2b92
Bryan Worrell <bworrell@mitre.org>
2014-10-24 12:25:11 -0400

    Cleanup and docs

--- sdv/validators/__init__.py
+++ sdv/validators/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
+
 import schematron
 from .schematron import (SchematronValidator, SchematronValidationResults)
 
