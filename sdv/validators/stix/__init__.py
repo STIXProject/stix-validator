ee32a0ea888739b3c6be6195b2ec19626689b528
Bryan Worrell <bworrell@mitre.org>
2014-12-08 09:11:04 -0500

    Disable warnings as these imports are done with purpose

--- sdv/validators/stix/__init__.py
+++ sdv/validators/stix/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-from .schema import STIXSchemaValidator
-from .profile import (STIXProfileValidator, ProfileValidationResults)
-from .best_practice import (
+from .schema import STIXSchemaValidator  # pylint: disable=unused-import
+from .profile import (STIXProfileValidator, ProfileValidationResults)  # pylint: disable=unused-import
+from .best_practice import (  # pylint: disable=unused-import
     STIXBestPracticeValidator, BestPracticeValidationResults
 )
