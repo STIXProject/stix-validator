4b3bd5ee9de6fe765b4b9dbd91ecb8a8138beb3e
Bryan Worrell <bworrell@mitre.org>
2015-02-11 21:59:10 -0500

    Removed unused import

--- sdv/validators/__init__.py
+++ sdv/validators/__init__.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-import json
-
 from .base import ValidationResults
 
 class ValidationErrorResults(ValidationResults):
