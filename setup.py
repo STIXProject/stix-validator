3bdd2573395ac0eb6860b54d55c2d5d47221a078
Greg Back <gback@mitre.org>
2015-04-02 09:44:31 -0400

    Standardize parts of setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2015 - The MITRE Corporation
+# Copyright (c) 2015 - The MITRE Corporation
 # For license information, see the LICENSE.txt file
 
+from os.path import abspath, dirname, join
 import sys
-from os.path import abspath, dirname, join, realpath
+
 from setuptools import setup, find_packages
 
 BASE_DIR = dirname(abspath(__file__))
